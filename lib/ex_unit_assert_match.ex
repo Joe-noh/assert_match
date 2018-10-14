c7a4963048c78ce06bd080933944b8892b07b359
Joe-noh <goflb.jh@gmail.com>
2018-10-14 15:21:06 +0900

    add docs to API functions

--- lib/ex_unit_assert_match.ex
+++ lib/ex_unit_assert_match.ex
@@ -7,6 +7,19 @@ defmodule ExUnitAssertMatch do
 
   alias ExUnitAssertMatch.Types
 
+  @doc """
+  Assert that given `data` match `type` specification.
+
+      alias ExUnitAssertMatch, as: Match
+
+      Match.assert Match.binary(), "Hello"
+      #=> pass
+
+      Match.assert Match.binary(), 123
+      #=> fail
+
+  This calls ExUnit `assert` macro internally.
+  """
   def assert(type, data, opts \\ [])
 
   def assert(type, data, opts = %ExUnitAssertMatch.Option{}) do
@@ -17,50 +30,103 @@ defmodule ExUnitAssertMatch do
     assert(type, data, struct(ExUnitAssertMatch.Option, opts))
   end
 
+  @doc """
+  Matches any map.
+  """
   def map() do
     %Types.Map{example: nil}
   end
 
+  @doc """
+  Matches a map that satisfies `example` specification.
+
+      alias ExUnitAssertMatch, as: Match
+
+      Match.assert Match.map(%{name: Match.binary()}), %{name: "John"}
+      #=> pass
+
+      Match.assert Match.map(%{name: Match.binary()}), %{name: nil}
+      #=> fail
+  """
   def map(example) do
     %Types.Map{example: example}
   end
 
+  @doc """
+  Matches any list.
+  """
   def list() do
     %Types.List{example: nil}
   end
 
+  @doc """
+  Matches a list that satisfies `example` specification.
+
+      alias ExUnitAssertMatch, as: Match
+
+      Match.assert Match.list_of(Match.binary()), ["1", "2"]
+      #=> pass
+
+      Match.assert Match.list_of(Match.binary()), ["1", 2]
+      #=> fail
+  """
   def list_of(example) do
     %Types.List{example: example}
   end
 
+  @doc """
+  Matches any atom.
+  """
   def atom() do
     %Types.Atom{}
   end
 
+  @doc """
+  Matches any binary.
+  """
   def binary() do
     %Types.Binary{}
   end
 
+  @doc """
+  Matches any integer.
+  """
   def integer() do
     %Types.Integer{}
   end
 
+  @doc """
+  Matches any float.
+  """
   def float() do
     %Types.Float{}
   end
 
+  @doc """
+  Matches if they are equal.
+  This uses `==` to compare them.
+  """
   def literal(example) do
     %Types.Literal{example: example}
   end
 
+  @doc """
+  Same as `literal/1`.
+  """
   def equal(example) do
     literal(example)
   end
 
+  @doc """
+  Matches if it satisfies `example` or equals to `nil`.
+  """
   def nullable(example) do
     %Types.Nullable{example: example}
   end
 
+  @doc """
+  Matches everything.
+  """
   def any() do
     %Types.Any{}
   end
